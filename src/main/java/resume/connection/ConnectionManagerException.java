fd8443931ca7712f58d6d8eca81437d9a3b5a6d5
rubashkin_mikhail@tut.by <pristijpribor@mail.ru>
2020-02-08 13:23:00 +0300

    ConnectionManager

---
+++ src/main/java/resume/connection/ConnectionManagerException.java
@@ -0,0 +1,7 @@
+package resume.connection;
+
+public class ConnectionManagerException extends RuntimeException {
+    public ConnectionManagerException(String message) {
+        super(message);
+    }
+}
---
+++ target/classes/liquibase/v3_Create_person_table.sql
@@ -0,0 +1,23 @@
+CREATE TABLE person2
+  (
+      ID int,
+      NAME2 varchar(100) NOT NULL,
+      SURNAME varchar(100) NOT NULL,
+      PATRONYMIC varchar(100) NOT NULL,
+      SEX varchar(50) NOT NULL,
+      Date_of_Birth date NOT NULL,
+      GITHUB_CONTACTS varchar(100),
+      EMAIL varchar(100),
+      TELEPHONE varchar(100),
+      SKYPE varchar(100),
+      LINKEDIN varchar(100),
+      GITHUB_TECHNOLOGY varchar(100),
+      SPRING_BOOT varchar(100),
+      HTML varchar(100),
+      JAVA_EE varchar(100),
+      JAVA_CORE varchar(100),
+      MAVEN varchar(100),
+      REST varchar(100),
+      SPRING varchar(100),
+      PRIMARY KEY (ID)
+  );
